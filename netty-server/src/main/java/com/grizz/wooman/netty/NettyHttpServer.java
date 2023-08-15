58ef9cd44b72f0974cba91ea3920bf727ff4bfe7
changmink <changmin043@gmail.com>
2023-08-15 19:51:28 +0900

    [#6] netty 서버 실습 2

--- netty-server/src/main/java/com/grizz/wooman/netty/NettyHttpServer.java
+++ netty-server/src/main/java/com/grizz/wooman/netty/NettyHttpServer.java
@@ -7,35 +7,37 @@ import io.netty.channel.DefaultEventLoopGroup;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.concurrent.EventExecutorGroup;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.FutureListener;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class NettyEchoServer {
+public class NettyHttpServer {
     public static void main(String[] args) {
         EventLoopGroup acceptEventGroup = new NioEventLoopGroup();
         EventLoopGroup readEventGroup = new NioEventLoopGroup();
         EventExecutorGroup eventExecutorGroup = new DefaultEventLoopGroup();
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(acceptEventGroup, readEventGroup);
-            serverBootstrap.channel(NioServerSocketChannel.class);
-            serverBootstrap.childHandler(new ChannelInitializer<>(){
+            var server = serverBootstrap.group(acceptEventGroup, readEventGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<>(){
                 @Override
                 protected void initChannel(Channel channel) throws Exception {
                     channel.pipeline().addLast(eventExecutorGroup, new LoggingHandler(LogLevel.INFO));
-                    channel.pipeline().addLast(new StringDecoder(), new StringEncoder(), new NettyEchoServerHandler());
+                    channel.pipeline().addLast(new HttpServerCodec(),
+                            new HttpObjectAggregator(1024*1024),
+                            new NettyHttpServerHandler()
+                    );
                 }
             });
 
-            serverBootstrap.bind(8080).sync()
+            server.bind(8080).sync()
                     .addListener(future -> {
                         if (future.isSuccess()) {
                             log.info("Success");
